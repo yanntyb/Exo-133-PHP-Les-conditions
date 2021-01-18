0be4c5f817c4bb411583a274134aa7c98962e99e
yann tyb <yanntyb.lbc@gmail.com>
2021-01-18 14:38:41 +0100

    a

--- index.php
+++ index.php
@@ -4,33 +4,32 @@
 $a = 50;
 $b = 10;
 
-       >    {
+if($a > $b){
     echo "Hello World!";
 }
 
 //Deuxieme ligne
-    {
+if($a !== $b){
     echo "<br><br>Hello World2";
 }
 
 //Troisieme ligne
-($a==b) {
+if($a==$b) {
     echo "<br><br>Oui";
 }
-
-{
+else{
     echo "<br><br>Non";
 }
 
 
 //Quatrieme ligne
 echo"<br><br>";
- ($a == $b) {
+ if($a === $b) {
     echo "1";
-}  ($a > $b) {
+} elseif($a > $b) {
     echo "2";
 }
-{
+else {
     echo "3";
 }
 
@@ -38,10 +37,16 @@ echo"<br><br>";
 //Cinquieme ligne
 echo"<br><br>";
 
- ($color) {
-    "red":
-    echo "Hello";
-   "green":
-    echo "Welcome";
+$color = "yellow";
+
+switch($color) {
+    case "red":
+        echo "Hello";
+        break;
+   case "green":
+        echo "Welcome";
+        break;
+   default:
+       echo "color !== red ou green";
 }
 
